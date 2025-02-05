458cd4972682ab2e878a832338ce097a0b0d95c9
Pedro Rodríguez Gallegos <pedro707rodriguez@gmail.com>
2025-02-05 19:39:23 +0100

    modified files: 	Tema 5/Ejercicios/Ejercicio 1/ej1.js 	Tema 5/Ejercicios/Ejercicio 1/index.html 	Tema 5/Ejercicios/Ejercicio 2/ej2.js 	Tema 5/Ejercicios/Ejercicio 2/index.html 	Tema 5/Ejercicios/Ejercicio 3/index.html 	Tema 5/Ejercicios/Ejercicio 4/index.html 	Tema 5/Ejercicios/Ejercicio 5/index.html 	Tema 5/Ejercicios/Ejercicio 6/index.html

--- Tema 5/Ejercicios/Ejercicio 1/ej1.js
+++
@@ -1,6 +0,0 @@
-function actualizarContador() {
-    const textarea = document.getElementById("miTextarea");
-    const contador = document.getElementById("contador");
-    const caracteresRestantes = 100 - textarea.value.length;
-    contador.textContent = `Caracteres restantes: ${caracteresRestantes}`;
-}
