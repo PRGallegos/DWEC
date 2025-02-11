67bcd5b3680df66883b6d24c295a1263381cbc95
Pedro <pedro707rodriguez@gmail.com>
2025-02-11 11:34:50 +0100

    Added all exercises from unidad 5

--- Tema 5/Ejercicio2/ej2.js
+++ Tema 5/Ejercicio2/ej2.js
@@ -1,24 +1,24 @@
 document.getElementById('modifyButton').addEventListener('click', function () {
     const contenido2 = document.getElementById('contenido_2');
 
-    // Crear nuevo párrafo
+    // Crear nuevo parrafo
     const newP = document.createElement('p');
-    newP.title = "Título del nuevo párrafo";
-    newP.appendChild(document.createTextNode("Este es la primera parte de mi nuevo párrafo"));
+    newP.title = "Titulo del nuevo parrafo";
+    newP.appendChild(document.createTextNode("Este es la primera parte de mi nuevo parrafo"));
     newP.appendChild(document.createElement('br'));
-    newP.appendChild(document.createTextNode("Este es la segunda parte de mi nuevo párrafo"));
+    newP.appendChild(document.createTextNode("Este es la segunda parte de mi nuevo parrafo"));
 
     // Crear texto en negrita
     const newB = document.createElement('b');
     newB.appendChild(document.createTextNode("Este es el texto en negrita."));
 
-    // Añadir elementos al párrafo contenido_2
+    // Añadir elementos al parrafo contenido_2
     contenido2.appendChild(newP);
     contenido2.appendChild(newB);
 
-    // Cambiar contenido del párrafo contenido_3
-    document.getElementById('contenido_3').textContent = "Este es el nuevo contenido del párrafo 3";
+    // Cambiar contenido del parrafo contenido_3
+    document.getElementById('contenido_3').textContent = "Este es el nuevo contenido del parrafo 3";
 
-    // Eliminar el párrafo contenido_1
+    // Eliminar el parrafo contenido_1
     document.getElementById('contenido_1').remove();
 });
